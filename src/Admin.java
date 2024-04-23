2a1298ff2564d2745a68d72d23d728861153952e
emir.bolin <emir.bolin@elev.ga.ntig.se>
2024-04-23 14:21:39 +0200

    Final commit 2.0. Removed unnecesarry attributes from Zookeeper and Admin class.

--- src/Admin.java
+++ src/Admin.java
@@ -1,9 +1,5 @@
 // Subclass of User
 public class Admin extends User {
-    // Attributes
-    private String name;
-    private String password;
-
     // Constructor
     public Admin(String name, String password) {
         super(name, password);
--- src/Zookeeper.java
+++ src/Zookeeper.java
@@ -1,9 +1,5 @@
 // Subclass of User
 public class Zookeeper extends User {
-    // Attributes
-    private String name;
-    private String password;
-
     // Constructor
     public Zookeeper(String name, String password) {
         super(name, password);
